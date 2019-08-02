2da5b5d0a165d2fdb7dc636a0d996ce4f4aa3237
dotieuthien <dotieuthien@dotieuthiens-MacBook-Pro.local>
2019-08-02 23:49:24 +0700

    Perspective Transform exp

--- upright_adjustment.py
+++ upright_adjustment.py
@@ -3,6 +3,7 @@ import cv2
 from pylsd.lsd import lsd
 import numpy as np
 from numpy.linalg import inv
+from sympy import *
 
 
 # Funcs for paper "rectification of planar targets using line segments"
@@ -10,12 +11,12 @@ from numpy.linalg import inv
 def line_detection(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     lines = lsd(gray)
-    for i in range(lines.shape[0]):
-        pt1 = (int(lines[i, 0]), int(lines[i, 1]))
-        pt2 = (int(lines[i, 2]), int(lines[i, 3]))
-        width = lines[i, 4]
-        cv2.line(image, pt1, pt2, (0, 0, 255), int(np.ceil(width / 2)))
-    cv2.imwrite('test_out.jpg', image)
+    # for i in range(lines.shape[0]):
+    #     pt1 = (int(lines[i, 0]), int(lines[i, 1]))
+    #     pt2 = (int(lines[i, 2]), int(lines[i, 3]))
+    #     width = lines[i, 4]
+    #     cv2.line(image, pt1, pt2, (0, 0, 255), int(np.ceil(width / 2)))
+    # cv2.imwrite('test_out.jpg', image)
     return lines
 
 
@@ -39,7 +40,7 @@ def cost_function(image, lines, theta=0, phi=0, gamma=0, f=1, epsilon=1):
                    [0, 0, 1]])
     # Composed rotation matrix with (RX, RY, RZ)
     R = np.dot(np.dot(RX, RY), RZ)
-    t = [0, 0, -max(h, w)]
+    t = np.array([])
     R[:, 2] = t
     # H function
     H = np.dot(K, R)
@@ -56,41 +57,230 @@ def cost_function(image, lines, theta=0, phi=0, gamma=0, f=1, epsilon=1):
         w = (line[0] + line[2])**2 + (line[1] + line[3])**2
         d = min(abs(u1[0] / u1[2] - v1[0] / v1[2]), abs(u1[1] / u1[2] - v1[1] / v1[2])) ** 2
         E = E + w * d
-    
+
     # The 2nd term
     a = max(h, w)
     F = (max(a, f) / min(a, f) -1) ** 2
 
-    retification = E + epsilon * F
-    return retification
+    rectification = E + epsilon * F
+    return rectification
 
 
-def derivative_d():
+def derivative_componets():
+    # Build graph of model
+    # Variales definition
+    f, theta, phi, gamma = symbols('f theta phi gamma')
+    u1, u2, u3 = symbols('u1 u2 u3')
+    v1, v2, v3 = symbols('v1 v2 v3')
+    # K matrix for internal paprameter of camera
     K = np.array([[f, 0, 0],
                   [0, f, 0],
                   [0, 0, 1]])
+    K1 = np.array([[1/f, 0, 0],
+                  [0, 1/f, 0],
+                  [0, 0, 1]])
     # Rotation matrices around the X, Y, and Z axis
     RX = np.array([[1, 0, 0],
-                   [0, np.cos(theta), -np.sin(theta)],
-                   [0, np.sin(theta), np.cos(theta)]])
+                   [0, cos(theta), -sin(theta)],
+                   [0, sin(theta), cos(theta)]])
     
-    RY = np.array([[np.cos(phi), 0, -np.sin(phi)],
+    RY = np.array([[cos(phi), 0, -sin(phi)],
                    [0, 1, 0],
-                   [np.sin(phi), 0, np.cos(phi)]])
+                   [sin(phi), 0, cos(phi)]])
     
-    RZ = np.array([[np.cos(gamma), -np.sin(gamma), 0],
-                   [np.sin(gamma), np.cos(gamma), 0],
+    RZ = np.array([[cos(gamma), -sin(gamma), 0],
+                   [sin(gamma), cos(gamma), 0],
                    [0, 0, 1]])
+
+    RXY = RX.dot(RY)
+    # Rotation matrix
+    R = RXY.dot(RZ)
+    # Inverse response funtion H
+    R1 = Matrix(R).T
+    H1 = K1.dot(R1)
+
+    # Compute derivative for each component
+    u = Matrix([u1, u2, u3])
+    H1u = H1.dot(u)
+    H1u_theta = diff(H1u, theta)
+    H1u_phi = diff(H1u, phi)
+    H1u_gamma = diff(H1u, gamma)
+    H1u_f = diff(H1u, f)
+
+    v = Matrix([v1, v2, v3])
+    H1v = H1.dot(v)
+    H1v_theta = diff(H1v, theta)
+    H1v_phi = diff(H1v, phi)
+    H1v_gamma = diff(H1v, gamma)
+    H1v_f = diff(H1v, f)
+    return H1u_theta, H1u_phi, H1u_gamma, H1u_f, H1v_theta, H1v_phi, H1v_gamma, H1v_f
+
+
+def gradient(lines, f_, theta_, phi_, gamma_):
+    H1u_theta, H1u_phi, H1u_gamma, H1u_f, H1v_theta, H1v_phi, H1v_gamma, H1v_f = derivative_componets()
+
+    h, w, _ = np.shape(image)
+
+    K = np.array([[f_, 0, 0],
+                  [0, f_, 0],
+                  [0, 0, 1]])
+    # Rotation matrices around the X, Y, and Z axis
+    RX = np.array([[1, 0, 0],
+                   [0, np.cos(theta_), -np.sin(theta_)],
+                   [0, np.sin(theta_), np.cos(theta_)]])
     
-    pass
+    RY = np.array([[np.cos(phi_), 0, -np.sin(phi_)],
+                   [0, 1, 0],
+                   [np.sin(phi_), 0, np.cos(phi_)]])
+    
+    RZ = np.array([[np.cos(gamma_), -np.sin(gamma_), 0],
+                   [np.sin(gamma_), np.cos(gamma_), 0],
+                   [0, 0, 1]])
+
+    # Composed rotation matrix with (RX, RY, RZ)
+    R = np.dot(np.dot(RX, RY), RZ)
+    t = [0, 0, -max(h, w)]
+    R[:, 2] = t
+    # H function
+    H = np.dot(K, R)
+    H1 = inv(H)
+
+    dE1 = 0
+    dE2 = 0
+    dE3 = 0
+    dE4 = 0
+
+    for line in lines:
+        u = [line[0], line[1], 1]
+        u1 = np.dot(H1, u)
+        v = [line[2], line[3], 1]
+        v1 = np.dot(H1, v)
+
+        # derivative for E (1st term)
+        w = (line[0] + line[2])**2 + (line[1] + line[3])**2
+        d = min(abs(u1[0] / u1[2] - v1[0] / v1[2]), abs(u1[1] / u1[2] - v1[1] / v1[2])) ** 2
+
+        if (abs(u1[0] / u1[2] - v1[0] / v1[2]) <= abs(u1[1] / u1[2] - v1[1] / v1[2])) and (u1[0] / u1[2] > v1[0] / v1[2]):
+            dd_H1u = np.array([1 / u1[2], -1 / u1[2], (-u1[0] + u1[1]) / (u1[2] ** 2)])
+            dE_tmp = w * 2 * d * dd_H1u
+            f, theta, phi, gamma = symbols('f theta phi gamma')
+            u1, u2, u3 = symbols('u1 u2 u3')
+            v1, v2, v3 = symbols('v1 v2 v3')
+
+            dE1 = dE1 + dE_tmp.dot(H1u_theta.subs({f:f_, theta:theta_, phi:phi_, gamma:gamma_, u1:u[0], u2:u[1], u3:u[2],
+                                                 v1:v[0], v2:v[1], v3:v[2]}))
+            dE2 = dE2 + dE_tmp.dot(H1u_phi.subs({f:f_, theta:theta_, phi:phi_, gamma:gamma_, u1:u[0], u2:u[1], u3:u[2],
+                                                 v1:v[0], v2:v[1], v3:v[2]}))
+            dE3 = dE3 + dE_tmp.dot(H1u_gamma.subs({f:f_, theta:theta_, phi:phi_, gamma:gamma_, u1:u[0], u2:u[1], u3:u[2],
+                                                 v1:v[0], v2:v[1], v3:v[2]}))
+            dE4 = dE4 + dE_tmp.dot(H1u_f.subs({f:f_, theta:theta_, phi:phi_, gamma:gamma_, u1:u[0], u2:u[1], u3:u[2],
+                                                 v1:v[0], v2:v[1], v3:v[2]}))
+
+        elif (abs(u1[0] / u1[2] - v1[0] / v1[2]) <= abs(u1[1] / u1[2] - v1[1] / v1[2])) and (u1[0] / u1[2] <= v1[0] / v1[2]):
+            dd_H1u = np.array([-1 / u1[2], 1 / u1[2], (u1[0] - u1[1]) / (u1[2] ** 2)])
+            dE_tmp = w * 2 * d * dd_H1u
+            f, theta, phi, gamma = symbols('f theta phi gamma')
+            u1, u2, u3 = symbols('u1 u2 u3')
+            v1, v2, v3 = symbols('v1 v2 v3')
+
+            dE1 = dE1 + dE_tmp.dot(H1u_theta.subs({f:f_, theta:theta_, phi:phi_, gamma:gamma_, u1:u[0], u2:u[1], u3:u[2],
+                                                 v1:v[0], v2:v[1], v3:v[2]}))
+            dE2 = dE2 + dE_tmp.dot(H1u_phi.subs({f:f_, theta:theta_, phi:phi_, gamma:gamma_, u1:u[0], u2:u[1], u3:u[2],
+                                                 v1:v[0], v2:v[1], v3:v[2]}))
+            dE3 = dE3 + dE_tmp.dot(H1u_gamma.subs({f:f_, theta:theta_, phi:phi_, gamma:gamma_, u1:u[0], u2:u[1], u3:u[2],
+                                                 v1:v[0], v2:v[1], v3:v[2]}))
+            dE4 = dE4 + dE_tmp.dot(H1u_f.subs({f:f_, theta:theta_, phi:phi_, gamma:gamma_, u1:u[0], u2:u[1], u3:u[2],
+                                                 v1:v[0], v2:v[1], v3:v[2]}))
+
+        elif (abs(u1[0] / u1[2] - v1[0] / v1[2]) > abs(u1[1] / u1[2] - v1[1] / v1[2])) and (u1[1] / u1[2] <= v1[1] / v1[2]):
+            dd_H1v = np.array([1 / v1[2], -1 / v1[2], (-v1[0] + v1[1]) / (v1[2] ** 2)])
+            dE_tmp = w * 2 * d * dd_H1v
+            f, theta, phi, gamma = symbols('f theta phi gamma')
+            u1, u2, u3 = symbols('u1 u2 u3')
+            v1, v2, v3 = symbols('v1 v2 v3')
+
+            dE1 = dE1 + dE_tmp.dot(H1v_theta.subs({f:f_, theta:theta_, phi:phi_, gamma:gamma_, u1:u[0], u2:u[1], u3:u[2],
+                                                 v1:v[0], v2:v[1], v3:v[2]}))
+            dE2 = dE2 + dE_tmp.dot(H1v_phi.subs({f:f_, theta:theta_, phi:phi_, gamma:gamma_, u1:u[0], u2:u[1], u3:u[2],
+                                                 v1:v[0], v2:v[1], v3:v[2]}))
+            dE3 = dE3 + dE_tmp.dot(H1v_gamma.subs({f:f_, theta:theta_, phi:phi_, gamma:gamma_, u1:u[0], u2:u[1], u3:u[2],
+                                                 v1:v[0], v2:v[1], v3:v[2]}))
+            dE4 = dE4 + dE_tmp.dot(H1v_f.subs({f:f_, theta:theta_, phi:phi_, gamma:gamma_, u1:u[0], u2:u[1], u3:u[2],
+                                                 v1:v[0], v2:v[1], v3:v[2]}))
+
+        else:
+            dd_H1v = np.array([-1 / v1[2], 1 / v1[2], (v1[0] - v1[1]) / (v1[2] ** 2)])
+            dE_tmp = w * 2 * d * dd_H1v
+            f, theta, phi, gamma = symbols('f theta phi gamma')
+            u1, u2, u3 = symbols('u1 u2 u3')
+            v1, v2, v3 = symbols('v1 v2 v3')
+
+            dE1 = dE1 + dE_tmp.dot(H1v_theta.subs({f:f_, theta:theta_, phi:phi_, gamma:gamma_, u1:u[0], u2:u[1], u3:u[2],
+                                                 v1:v[0], v2:v[1], v3:v[2]}))
+            dE2 = dE2 + dE_tmp.dot(H1v_phi.subs({f:f_, theta:theta_, phi:phi_, gamma:gamma_, u1:u[0], u2:u[1], u3:u[2],
+                                                 v1:v[0], v2:v[1], v3:v[2]}))
+            dE3 = dE3 + dE_tmp.dot(H1v_gamma.subs({f:f_, theta:theta_, phi:phi_, gamma:gamma_, u1:u[0], u2:u[1], u3:u[2],
+                                                 v1:v[0], v2:v[1], v3:v[2]}))
+            dE4 = dE4 + dE_tmp.dot(H1v_f.subs({f:f_, theta:theta_, phi:phi_, gamma:gamma_, u1:u[0], u2:u[1], u3:u[2],
+                                                 v1:v[0], v2:v[1], v3:v[2]}))
+
+    dE = [dE1, dE2, dE3, dE4]
+    return dE
 
 
-def optimizer():
-    pass
+def gradient_descent_optimizer(image, max_iters, learning_rate):
+    init_f = 100
+    init_theta = 0
+    init_phi = 0
+    init_gamma = 0
+    for i in range(max_iters):
+        # Feed forward
+        lines = line_detection(image)
+        # Loss function
+        rectification = cost_function(image, lines)
+        print(rectification)
+        # Compute gradient
+        f, theta, phi, gamma = gradient(lines, init_f, init_theta, init_phi, init_gamma)
+        init_f = np.float32(init_f - f)
+        init_theta = np.float32(init_theta - theta * np.pi / 180)
+        init_phi = np.float32(init_phi - phi * np.pi / 180)
+        init_gamma = np.float32(init_gamma - gamma * np.pi / 180)
+
+        h, w, _ = np.shape(image)
+
+        K = np.array([[init_f, 0, 0],
+                    [0, init_f, 0],
+                    [0, 0, 1]])
+        # Rotation matrices around the X, Y, and Z axis
+        RX = np.array([[1, 0, 0],
+                    [0, np.cos(init_theta), -np.sin(init_theta)],
+                    [0, np.sin(init_theta), np.cos(init_theta)]])
+        
+        RY = np.array([[np.cos(init_phi), 0, -np.sin(init_phi)],
+                    [0, 1, 0],
+                    [np.sin(init_phi), 0, np.cos(init_phi)]])
+        
+        RZ = np.array([[np.cos(init_gamma), -np.sin(init_gamma), 0],
+                    [np.sin(init_gamma), np.cos(init_gamma), 0],
+                    [0, 0, 1]])
+
+        # Composed rotation matrix with (RX, RY, RZ)
+        R = np.dot(np.dot(RX, RY), RZ)
+        # t = [0, 0, -max(h, w)]
+        # R[:, 2] = t
+        # H function
+        H = np.dot(K, R)
+        print(H)
+        H1 = np.linalg.inv(H)
+        print(H1)
+        image = cv2.warpPerspective(image.copy(), np.float32(H1), (w, h))
+        cv2.imwrite('image_out_p.png', image)
 
 
-# Test
-image = cv2.imread('test_images/4.jpg')
-lines = line_detection(image)
-retification = cost_function(image, lines)
-print(retification)
+
+if __name__ == '__main__':
+    image = cv2.imread('test_images/4.jpg')
+    # lines = line_detection(image)
+    # retification = cost_function(image, lines)
+    # derivative_componets()
+    gradient_descent_optimizer(image, 3, 0.1)
